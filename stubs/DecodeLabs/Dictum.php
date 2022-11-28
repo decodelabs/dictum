058aeb7fa54dd9d2c15427bc7633a7521030c215
Tom Wright <tom@inflatablecookie.com>
2022-11-28 15:11:10 +0000

    Moved plugin interfaces to Cosmos
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- stubs/DecodeLabs/Dictum.php
+++ stubs/DecodeLabs/Dictum.php
@@ -5,8 +5,8 @@
  */
 namespace DecodeLabs;
 
-use DecodeLabs\Veneer\Proxy;
-use DecodeLabs\Veneer\ProxyTrait;
+use DecodeLabs\Veneer\Proxy as Proxy;
+use DecodeLabs\Veneer\ProxyTrait as ProxyTrait;
 use DecodeLabs\Dictum\Context as Inst;
 use DecodeLabs\Dictum\Plugins\Number as NumberPlugin;
 use DecodeLabs\Dictum\Plugins\Time as TimePlugin;
