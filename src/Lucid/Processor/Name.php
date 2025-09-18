0d594ea1ef9cea060f841a7415217a5774c002b7
Tom Wright <tom@inflatablecookie.com>
2025-09-18 15:39:38 +0100

    Ported Lucid constraints
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Lucid/Processor/Name.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @package Dictum
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Lucid\Processor;
+
+use DecodeLabs\Coercion;
+use DecodeLabs\Dictum;
+use DecodeLabs\Lucid\Processor;
+use DecodeLabs\Lucid\ProcessorTrait;
+
+/**
+ * @implements Processor<string>
+ */
+class Name implements Processor
+{
+    /**
+     * @use ProcessorTrait<string>
+     */
+    use ProcessorTrait;
+
+    public const array OutputTypes = ['string:name'];
+
+    public function getDefaultConstraints(): array
+    {
+        return [
+            'trim' => true
+        ];
+    }
+
+    /**
+     * Convert prepared value to string or null
+     */
+    public function coerce(
+        mixed $value
+    ): ?string {
+        if ($value === null) {
+            return null;
+        }
+
+        $string = Coercion::asString($value);
+        return Dictum::name($string);
+    }
+}
---
+++ src/Lucid/Processor/PathSlug.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @package Dictum
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Lucid\Processor;
+
+use DecodeLabs\Coercion;
+use DecodeLabs\Dictum;
+use DecodeLabs\Lucid\Processor;
+use DecodeLabs\Lucid\ProcessorTrait;
+
+/**
+ * @implements Processor<string>
+ */
+class PathSlug implements Processor
+{
+    /**
+     * @use ProcessorTrait<string>
+     */
+    use ProcessorTrait;
+
+    public const array OutputTypes = ['string:slug', 'string:pathSlug'];
+
+    /**
+     * Convert prepared value to string or null
+     */
+    public function coerce(
+        mixed $value
+    ): ?string {
+        if ($value === null) {
+            return null;
+        }
+
+        $string = Coercion::asString($value);
+        return Dictum::pathSlug($string);
+    }
+}
